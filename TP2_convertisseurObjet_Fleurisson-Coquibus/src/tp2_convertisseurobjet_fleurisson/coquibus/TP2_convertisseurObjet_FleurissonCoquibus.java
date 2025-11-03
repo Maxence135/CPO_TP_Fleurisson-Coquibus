e093ba1dd0de5bf02c2e4a65b27b07ff84301e4f
Maxence <maxence.fleurisson-coquibus@epfedu.fr>
2025-11-03 15:24:52 +0100

    Ajout de TP2_Convertisseur

--- TP2_convertisseurObjet_Fleurisson-Coquibus/src/tp2_convertisseurobjet_fleurisson/coquibus/TP2_convertisseurObjet_FleurissonCoquibus.java
+++ TP2_convertisseurObjet_Fleurisson-Coquibus/src/tp2_convertisseurobjet_fleurisson/coquibus/TP2_convertisseurObjet_FleurissonCoquibus.java
@@ -4,25 +4,70 @@
  */
 package tp2_convertisseurobjet_fleurisson.coquibus;
 
-/**
- *
- * @author maxen
+/*
+Nom du programme : Convertisseur
+Auteur : Maxence Fleurisson-Coquibus
+Role : Familiarisation avec l'environnement
+Date : 03/11/2025
  */
+import java.util.Scanner;
+
 public class TP2_convertisseurObjet_FleurissonCoquibus {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Convertisseur conv1 = new Convertisseur();
+        Scanner sc = new Scanner(System.in);
+       
+        Convertisseur conv = new Convertisseur();
 
-        double tempC = 25;
-        double tempK = conv1.CelciusVersKelvin(tempC);
-        System.out.println(tempC + "°C = " + tempK + " K");
+        System.out.println("=== Convertisseur de températures ===");
+        System.out.println("1 : Celsius vers Kelvin");
+        System.out.println("2 : Kelvin vers Celsius");
+        System.out.println("3 : Fahrenheit vers Celsius");
+        System.out.println("4 : Celsius vers Fahrenheit");
+        System.out.println("5 : Kelvin vers Fahrenheit");
+        System.out.println("6 : Fahrenheit vers Kelvin");
+        System.out.print("Choisissez une conversion : ");
+        int choix = sc.nextInt();
 
-        double tempF = conv1.CelciusVersFarenheit(tempC);
-        System.out.println(tempC + "°C = " + tempF + " °F");
+        System.out.print("Entrez la température à convertir : ");
+        double temp = sc.nextDouble();
 
-        System.out.println(conv1); 
+        double resultat = 0;
+
+        switch (choix) {
+            case 1:
+                resultat = conv.CelciusVersKelvin(temp);
+                System.out.println(temp + " °C = " + resultat + " K");
+                break;
+            case 2:
+                resultat = conv.KelvinVersCelcius(temp);
+                System.out.println(temp + " K = " + resultat + " °C");
+                break;
+            case 3:
+                resultat = conv.FarenheitVersCelcius(temp);
+                System.out.println(temp + " °F = " + resultat + " °C");
+                break;
+            case 4:
+                resultat = conv.CelciusVersFarenheit(temp);
+                System.out.println(temp + " °C = " + resultat + " °F");
+                break;
+            case 5:
+                resultat = conv.KelvinVersFarenheit(temp);
+                System.out.println(temp + " K = " + resultat + " °F");
+                break;
+            case 6:
+                resultat = conv.FarenheitVersKelvin(temp);
+                System.out.println(temp + " °F = " + resultat + " K");
+                break;
+            default:
+                System.out.println("Choix invalide !");
+        }
+
+        System.out.println(conv);
     }
 }
+
+
