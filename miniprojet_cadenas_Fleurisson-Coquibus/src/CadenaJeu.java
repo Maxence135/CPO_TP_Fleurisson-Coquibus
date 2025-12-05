45423d2a1207739d1c12e2753b0b49e50d02c412
Maxence <maxence.fleurisson-coquibus@epfedu.fr>
2025-12-05 15:31:35 +0100

    Ajout de miniprojet_cadenas : finition potentiel de la classe CadenaJeu

--- miniprojet_cadenas_Fleurisson-Coquibus/src/CadenaJeu.java
+++ miniprojet_cadenas_Fleurisson-Coquibus/src/CadenaJeu.java
@@ -8,18 +8,53 @@
  * @author maxen
  */
 public class CadenaJeu {
+
     private int[] combinaisonSecrete = new int[4];
     private int tentatives = 0;
     private int maxTentatives = 5;
-    public CadenaJeu(){
+
+    public CadenaJeu() {
         genererCombinaison();
     }
-    
+
     //La methode creation de la combinaison
-    public void genererCombinaison(){
-        for(int i=0; i<4; i++){
-            combinaisonSecrete[i]=(int)(Math.random()*10);
+    public void genererCombinaison() {
+        for (int i = 0; i < 4; i++) {
+            combinaisonSecrete[i] = (int) (Math.random() * 10);
         }
     }
+
+    //On test la combinaison
+    public Resultat test(int[] essai) {
+        tentatives++;
+        int exact = 0;
+        int trophaut = 0;
+        int tropbas = 0;
+        for (int i = 0; i < 4; i++) {
+            if (essai[i] == combinaisonSecrete[i]) {
+                exact++;
+            } else if (essai[i] > combinaisonSecrete[i]) {
+                trophaut++;
+            } else {
+                tropbas++;
+            }
+
+        }
+        return new Resultat(exact,trophaut,tropbas);
+    }
     
+    //On regarde si le jouer a utiliser les tentatives
+    public boolean isFini(){
+        return tentatives>=maxTentatives;
+    }
+    
+    public int getTentative(){
+        return tentatives;
+    }
+    
+    //Renitialisation du jeu
+    public void reset(){
+        tentatives =0;
+        genererCombinaison();
+    }
 }
