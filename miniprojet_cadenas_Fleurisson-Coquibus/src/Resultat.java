e5f015b9017a226a59026729c84567b100e76ecf
Maxence <maxence.fleurisson-coquibus@epfedu.fr>
2025-12-05 14:55:32 +0100

    Ajout de miniprojet_cadenas : finition de la classe Resultat

---
+++ miniprojet_cadenas_Fleurisson-Coquibus/src/Resultat.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author maxen
+ */
+public class Resultat {
+    public int exact;
+    public int haut;
+    public int bas;
+
+    public Resultat(int exact, int haut, int bas) {
+        this.exact = exact;
+        this.haut = haut;
+        this.bas = bas;
+    }
+
+    public int getExact() {
+        return exact;
+    }
+
+    public int getHaut() {
+        return haut;
+    }
+
+    public int getBas() {
+        return bas;
+    }
+    
+    
+}
