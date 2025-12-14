99d3b534e120931a427e0cea28c617432a49ec4a
Maxence <maxence.fleurisson-coquibus@epfedu.fr>
2025-12-14 19:23:01 +0100

    Ajout de miniprojet_cadenas Version 2 : Interface d'accueil terminé

--- miniprojet_cadenas_Fleurisson-Coquibus/src/FenetreAccueil.java
+++ miniprojet_cadenas_Fleurisson-Coquibus/src/FenetreAccueil.java
@@ -17,9 +17,7 @@ public class FenetreAccueil extends javax.swing.JFrame {
     public FenetreAccueil() {
         initComponents();
     }
-    public void run(){
-        new FenetreAccueil().setVisible(true);
-    }
+
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -77,7 +75,7 @@ public class FenetreAccueil extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[]){
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
